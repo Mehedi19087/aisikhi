3546c8089272a9ec19cb24af1eed6cea65ce06a7
sunmendi <mehedimahbub706@example.com>
2026-06-17 13:38:25 +0600

    remove auth of frontend redirect url

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ from .views import CurrentUserView, GoogleAuthURLView, GoogleCallbackView
 
 urlpatterns = [
     path("auth/google/url/", GoogleAuthURLView.as_view(), name="google-auth-url"),
-    path("auth/google/callback/", GoogleCallbackView.as_view(), name="google-callback"),
+    path("google/callback/", GoogleCallbackView.as_view(), name="google-callback"),
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("auth/me/", CurrentUserView.as_view(), name="current-user"),
 ]
